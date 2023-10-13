ff1544a7a88abc883e54c289cd9592397247b85b
staj_Eren Özmen <staj_eren.ozmen@hititdc.com>
2023-10-13 14:00:21 +0300

    ilk react commiti

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    test
+    <img src='/logo192.png'></img>
+    <img src='{logo.svg}'></img>
+    </div>
+  );
+}
+
+export default App;
